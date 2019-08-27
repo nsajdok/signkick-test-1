009a0138ef5acf9250b8785cd3806d8989eeb9f3
Norbert Sajdok <norbert.sajdok@tsh.io>
2019-08-27 12:07:48 +0200

    Switch static form to reactive form by using FormBuilder

--- src/app/form-field-overview-example.ts
+++ src/app/form-field-overview-example.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { UsernameService } from './username.service';
 import { UserService } from './user.service';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'form-field-overview-example',
@@ -9,10 +9,20 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['form-field-overview-example.css'],
 })
 export class FormFieldOverviewExample {
+  form: FormGroup;
+
   constructor(
     private _user: UserService, 
     private _formBuilder: FormBuilder,
     private _username: UsernameService, 
-  ) {}
+  ) {
+    this.form = this._formBuilder.group({
+      firstName: [''],
+      lastName: [''],
+      userName: [''],
+      description: [''],
+      gender: ['']
+    });
+  }
 }
 
